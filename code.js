84cdc195ee4391e575714437f1cd074a18f33621
Boladek <55157480+Boladek@users.noreply.github.com>
2020-08-22 10:57:02 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
+var myVar = setInterval(myTimer, 1000);
+function myTimer(){
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    document.getElementById("time-bar").innerHTML = t;
+}
+
 var guess1 = Math.floor(Math.random() * 90) + 1;
 var guess2 = Math.floor(Math.random() * 90) + 1;
 var guess3 = Math.floor(Math.random() * 90) + 1;
@@ -20,23 +27,7 @@ var results = guess1 + "-" + guess2 + "-" + guess3;
 
 var num1 = document.getElementById("num1").value;
 var num2 = document.getElementById("num2").value;
-var num3 = document.getElementById("num3").value;
+var num3 = document.getElementById("num3").value
 
-var countDownTime = new Date("05:00").getTime();
-var x = setInterval(function() {
 
-    var now = new Date().getTime();
-    
-    var distance = countDownTime - now;
-    // var days = Math.floor(distance/(1000 * 60 * 60 * 24));
-    // var hours = Math.floor((distance % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60))/(1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60))/ 1000);
 
-    document.getElementById("timer").innerHTML = minutes + "m : " + seconds + "s ";
-
-    if (distance < 0){
-        clearInterval(x);
-    document.getElementById("display-results").innerHTML = results;
-    } 
-}, 1000);
